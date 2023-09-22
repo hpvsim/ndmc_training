439686a2d41fb78b42d4b861e2a30d27686d4b15
jamiecohen <jamie.alex.cohen@gmail.com>
2023-09-22 08:52:15 +0530

    update version

--- day3/vaccination/version.py
+++ day3/vaccination/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '0.1.0'
-__versiondate__ = '2022-12-12'
-__license__ = f'HPVsim HPV Therapeutic Vaccine Analyses {__version__} ({__versiondate__}) — © 2022 by IDM'
+__versiondate__ = '2023-09-22'
+__license__ = f'HPVsim HPV NDMC Analyses {__version__} ({__versiondate__}) — © 2023 by IDM'
